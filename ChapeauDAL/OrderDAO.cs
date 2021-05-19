70263d51bbe113271b169393202de6aa770627f0
amrish991 <68712252+amrish991@users.noreply.github.com>
2021-05-19 13:45:03 +0200

    Update DAO, UI,Logic, model for order

--- ChapeauDAL/OrderDAO.cs
+++ ChapeauDAL/OrderDAO.cs
@@ -9,37 +9,36 @@ using System.Threading.Tasks;
 
 namespace ChapeauDAL
 {
-    class OrderDAO : BaseDao
+   public class OrderDAO : BaseDao
     {
-        public int test;
-        //needs to be edited
         // This method passes the query to the ExecuteSelectQuery METHOD
-        public List<MenuItem> GetAllMenuItems()
+        public List<Order> GetAllOrders()
         {
-            string query = "SELECT [item_id], [menu_type], [item_type], [item_name], [item_price], [stock], [vat] FROM MENU_ITEM;";
+            string query = "SELECT [order_id], [item_code], [table_code], [quantity], [order_time], [order_price], [order_status], [employee_code] FROM ORDER;";
             SqlParameter[] sqlParameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
 
         // Read the selected tablerows from the database and add it to the list
-        private List<MenuItem> ReadTables(DataTable dataTable)
+        private List<Order> ReadTables(DataTable dataTable)
         {
-            List<MenuItem> menuItems = new List<MenuItem>();
+            List<Order> orders = new List<Order>();
 
             foreach (DataRow dr in dataTable.Rows)
             {
+                int order_id = (int)dr["order_id"];
                 int item_id = (int)dr["item_id"];
-                string menu_type = (string)dr["menu_type"];
-                string item_type = (string)dr["item_type"];
-                string item_name = (string)dr["item_name"];
-                double item_price = (double)dr["item_price"];
-                int stock = (int)dr["stock"];
-                int vat = (int)dr["vat"];
+                int table_code = (int)dr["table_code"];
+               int quantity = (int)dr["quantity"];
+               DateTime order_time = (DateTime)dr["order_time"];
+                double order_price = (double)dr["order_price"];
+                int order_status = (int)dr["order_status"];
+                int employee_code = (int)dr["employee_code"];
 
-                MenuItem menuItem = new MenuItem(item_id, menu_type, item_type, item_name, item_price, stock, vat);
-                menuItems.Add(menuItem);
+                Order order = new Order(order_id, item_id, table_code, quantity, order_time, order_price, order_status, employee_code);
+               orders.Add(order);
             }
-            return menuItems;
+            return orders;
         }
     }
 }
--- ChapeauUI/OrderForm.cs
+++ ChapeauUI/OrderForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChapeauLogic;
+using ChapeauModel;
 
 namespace ChapeauUI
 {
@@ -19,7 +21,40 @@ namespace ChapeauUI
 
         private void OrderForm_Load(object sender, EventArgs e)
         {
+            WinAPI.AnimateWindow(this.Handle, 2000, WinAPI.BLEND);
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listViewOrder.Clear();
+        }
+
+        private void listViewOrder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OrderService orderService = new OrderService();
+            List<Order> orders = orderService.GetAllOrders();
+
+            foreach (var Item in orders)
+            {
+                 ListViewItem li = new ListViewItem(Item.order_id.ToString());
+                li.SubItems.Add(Item.item_code.ToString());
+                li.SubItems.Add(Item.table_code.ToString());
+                li.SubItems.Add(Item.quantity.ToString());
+                li.SubItems.Add(Item.order_time.ToString());
+                li.SubItems.Add(Item.order_price.ToString());
+                li.SubItems.Add(Item.order_price.ToString());
+                li.SubItems.Add(Item.order_price.ToString());
+                li.SubItems.Add(Item.order_status.ToString());
+                li.SubItems.Add(Item.employee_code.ToString());
+                listViewOrder.Items.Add(li);
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
