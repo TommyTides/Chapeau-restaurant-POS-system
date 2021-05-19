592609516cd64462408b7c187216f11801b41c0c
tommykarjamenawi <tommy_12345@live.nl>
2021-05-19 12:37:45 +0200

    pull/push

--- ChapeauUI/Program.cs
+++ ChapeauUI/Program.cs
@@ -16,8 +16,7 @@ namespace ChapeauUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Menu());
-            Application.Run(new TakingOrderForm());
+            Application.Run(new Menu());
         }
     }
 }
