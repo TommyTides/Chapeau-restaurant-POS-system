e6e390f4774ab734b42979d5e62333f875d44e31
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2021-05-25 13:14:51 +0200

    table page code for getting tables and status

---
+++ ChapeauUI/TablePage.Designer.cs
@@ -0,0 +1,366 @@
+﻿
+namespace ChapeauUI
+{
+    partial class TablePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblrestaurantview = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btntable1 = new System.Windows.Forms.Button();
+            this.btntable2 = new System.Windows.Forms.Button();
+            this.btntable3 = new System.Windows.Forms.Button();
+            this.btntable4 = new System.Windows.Forms.Button();
+            this.btntable5 = new System.Windows.Forms.Button();
+            this.btntable6 = new System.Windows.Forms.Button();
+            this.btntable7 = new System.Windows.Forms.Button();
+            this.btntable8 = new System.Windows.Forms.Button();
+            this.btntable10 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.lblemployee = new System.Windows.Forms.Label();
+            this.btntable9 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(150)))), ((int)(((byte)(44)))));
+            this.panel1.Controls.Add(this.lblemployee);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.lblrestaurantview);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 162);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChapeauUI.Properties.Resources.logo_chapeau;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 195);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblrestaurantview
+            // 
+            this.lblrestaurantview.AutoSize = true;
+            this.lblrestaurantview.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblrestaurantview.ForeColor = System.Drawing.Color.White;
+            this.lblrestaurantview.Location = new System.Drawing.Point(278, 64);
+            this.lblrestaurantview.Name = "lblrestaurantview";
+            this.lblrestaurantview.Size = new System.Drawing.Size(267, 39);
+            this.lblrestaurantview.TabIndex = 1;
+            this.lblrestaurantview.Text = "Restaurant View";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Table Color Key";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Green;
+            this.panel2.Location = new System.Drawing.Point(21, 243);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(55, 37);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Table Free";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Location = new System.Drawing.Point(257, 243);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(55, 37);
+            this.panel3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(331, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(361, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Table Occupied";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Aqua;
+            this.panel4.Location = new System.Drawing.Point(498, 243);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(55, 37);
+            this.panel4.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(579, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(618, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Table Reserved";
+            // 
+            // btntable1
+            // 
+            this.btntable1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable1.Location = new System.Drawing.Point(155, 345);
+            this.btntable1.Name = "btntable1";
+            this.btntable1.Size = new System.Drawing.Size(84, 62);
+            this.btntable1.TabIndex = 9;
+            this.btntable1.Text = "Table 1";
+            this.btntable1.UseVisualStyleBackColor = true;
+            // 
+            // btntable2
+            // 
+            this.btntable2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable2.Location = new System.Drawing.Point(440, 345);
+            this.btntable2.Name = "btntable2";
+            this.btntable2.Size = new System.Drawing.Size(84, 62);
+            this.btntable2.TabIndex = 10;
+            this.btntable2.Text = "Table 2";
+            this.btntable2.UseVisualStyleBackColor = true;
+            // 
+            // btntable3
+            // 
+            this.btntable3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable3.Location = new System.Drawing.Point(155, 477);
+            this.btntable3.Name = "btntable3";
+            this.btntable3.Size = new System.Drawing.Size(84, 62);
+            this.btntable3.TabIndex = 11;
+            this.btntable3.Text = "Table 3";
+            this.btntable3.UseVisualStyleBackColor = true;
+            // 
+            // btntable4
+            // 
+            this.btntable4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable4.Location = new System.Drawing.Point(440, 477);
+            this.btntable4.Name = "btntable4";
+            this.btntable4.Size = new System.Drawing.Size(84, 62);
+            this.btntable4.TabIndex = 12;
+            this.btntable4.Text = "Table 4";
+            this.btntable4.UseVisualStyleBackColor = true;
+            // 
+            // btntable5
+            // 
+            this.btntable5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable5.Location = new System.Drawing.Point(155, 620);
+            this.btntable5.Name = "btntable5";
+            this.btntable5.Size = new System.Drawing.Size(84, 62);
+            this.btntable5.TabIndex = 13;
+            this.btntable5.Text = "Table 5";
+            this.btntable5.UseVisualStyleBackColor = true;
+            // 
+            // btntable6
+            // 
+            this.btntable6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable6.Location = new System.Drawing.Point(440, 620);
+            this.btntable6.Name = "btntable6";
+            this.btntable6.Size = new System.Drawing.Size(84, 62);
+            this.btntable6.TabIndex = 14;
+            this.btntable6.Text = "Table 6";
+            this.btntable6.UseVisualStyleBackColor = true;
+            // 
+            // btntable7
+            // 
+            this.btntable7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable7.Location = new System.Drawing.Point(155, 770);
+            this.btntable7.Name = "btntable7";
+            this.btntable7.Size = new System.Drawing.Size(84, 62);
+            this.btntable7.TabIndex = 15;
+            this.btntable7.Text = "Table 7";
+            this.btntable7.UseVisualStyleBackColor = true;
+            // 
+            // btntable8
+            // 
+            this.btntable8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable8.Location = new System.Drawing.Point(440, 770);
+            this.btntable8.Name = "btntable8";
+            this.btntable8.Size = new System.Drawing.Size(84, 62);
+            this.btntable8.TabIndex = 16;
+            this.btntable8.Text = "Table 8";
+            this.btntable8.UseVisualStyleBackColor = true;
+            // 
+            // btntable10
+            // 
+            this.btntable10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable10.Location = new System.Drawing.Point(440, 899);
+            this.btntable10.Name = "btntable10";
+            this.btntable10.Size = new System.Drawing.Size(84, 62);
+            this.btntable10.TabIndex = 17;
+            this.btntable10.Text = "Table 10";
+            this.btntable10.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(593, 64);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(136, 66);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Back to Menu";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // lblemployee
+            // 
+            this.lblemployee.AutoSize = true;
+            this.lblemployee.Location = new System.Drawing.Point(497, 21);
+            this.lblemployee.Name = "lblemployee";
+            this.lblemployee.Size = new System.Drawing.Size(38, 17);
+            this.lblemployee.TabIndex = 3;
+            this.lblemployee.Text = "------";
+            // 
+            // btntable9
+            // 
+            this.btntable9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntable9.Location = new System.Drawing.Point(155, 899);
+            this.btntable9.Name = "btntable9";
+            this.btntable9.Size = new System.Drawing.Size(84, 62);
+            this.btntable9.TabIndex = 18;
+            this.btntable9.Text = "Table 9";
+            this.btntable9.UseVisualStyleBackColor = true;
+            // 
+            // TablePage
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(752, 985);
+            this.Controls.Add(this.btntable9);
+            this.Controls.Add(this.btntable10);
+            this.Controls.Add(this.btntable8);
+            this.Controls.Add(this.btntable7);
+            this.Controls.Add(this.btntable6);
+            this.Controls.Add(this.btntable5);
+            this.Controls.Add(this.btntable4);
+            this.Controls.Add(this.btntable3);
+            this.Controls.Add(this.btntable2);
+            this.Controls.Add(this.btntable1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TablePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TablePage";
+            this.Load += new System.EventHandler(this.TablePage_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label lblrestaurantview;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btntable1;
+        private System.Windows.Forms.Button btntable2;
+        private System.Windows.Forms.Button btntable3;
+        private System.Windows.Forms.Button btntable4;
+        private System.Windows.Forms.Button btntable5;
+        private System.Windows.Forms.Button btntable6;
+        private System.Windows.Forms.Button btntable7;
+        private System.Windows.Forms.Button btntable8;
+        private System.Windows.Forms.Button btntable10;
+        private System.Windows.Forms.Label lblemployee;
+        private System.Windows.Forms.Button btntable9;
+    }
+}
