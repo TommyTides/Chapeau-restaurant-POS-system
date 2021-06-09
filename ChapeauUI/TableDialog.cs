8653774578afb4b238299aba5d17e969492bbe97
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2021-06-09 14:35:20 +0200

    created a dialog box for table page

---
+++ ChapeauUI/TableDialog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChapeauModel;
+
+namespace ChapeauUI
+{
+    public partial class TableDialog : Form
+    {
+        Table table;
+        public TableDialog(Table table)
+        {
+            this.table = table;
+            InitializeComponent();
+        }
+    }
+}
